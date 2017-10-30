274c1d44a8b3b0c8362c4de5be016c055eefe3fa
David Lacy <david.lacy@temple.edu>
2017-10-30 07:15:28 -0400

    re-added merge function

--- cluster.js
+++ cluster.js
@@ -40,7 +40,7 @@ var width = 960,
     maxRadius = 12;
 
 var n = 200, // total number of circles
-    m = 14; // number of distinct clusters
+    m = groups.length; // number of distinct clusters
 
 var color = d3.scaleOrdinal(d3.schemeCategory10)
     .domain(d3.range(m));
@@ -61,8 +61,6 @@ var nodes = d3.range(n).map(function() {
 var graph;
 var nodes = [];
 var unique_nodes = [];
-
-
 
 var forceCollide = d3.forceCollide()
     .radius(function(d) { return d.radius + 1.5; })
@@ -87,91 +85,94 @@ var icon = svg.selectAll("icon");
 
 function update() {
 	// EXIT
-	icon.exit().remove();
-    circle.exit().remove();
+	//icon.exit().remove();
+
 
     simulation.force("charge", d3.forceManyBody())
         .nodes(nodes)
         .force("cluster", forceCluster)
         .on("tick", tick);
 
+    //;
+    //circle.exit().remove();
     circle = circle.data(nodes)
         .enter().append("circle")
-            .attr("r", function(d) { return d.radius; })
-            .style("fill", function(d) { return color(d.cluster); })
-            .call(d3.drag()
-                .on("start", dragstarted)
-                .on("drag", dragged)
-                .on("end", dragended)
-            )
-            .on("click", function(d) {
-                req = d3.request("http://45.33.93.64/blazegraph/sparql")
-                .header("Accept", "application/json")
-                .header("Content-Type", "application/sparql-query")
-                .response(function(xhr) {
-                    return xhr.responseText;
-                })
-                .post(getRelationships(d.id), function(data) {
-                    //console.log(data);
-                    sparql_response = JSON.parse(data);
-                    results = sparql_response.results.bindings;
-                    new_nodes = [];
-                    new_links = [];
-                    console.log(results);
-                    for (k in results) {
-                        new_node = results[k];
-                        if (new_node.s) {
-                            id = new_node.s.value;
-                            label = new_node.s_label.value;
-                            type = new_node.s_type.value;
-                        } else {
-                            id = new_node.o.value;
-                            label = new_node.o_label.value;
-                            type = new_node.o_type.value
-                        }
-                        console.log(id);
-                        console.log(label);
-                        console.log(type);
-
-                        i = groups[type].id;
-                        r = 20;
-
-                        new_nodes[id] = {
-                            "id": id,
-                            "group": groups[type].id,
-                            "type": type,
-                            "img": groups[type].img,
-                            "label": label,
-                            "radius": r,
-                            "cluster": i
-                        };
-                        if (!clusters[i] || (r > clusters[i].radius)) clusters[i] = new_nodes[id];
+        .attr("r", function(d) { return d.radius; })
+        .style("fill", function(d) { return color(d.cluster); })
+        .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended)
+        )
+        .on("click", function(d) {
+            req = d3.request("http://45.33.93.64/blazegraph/sparql")
+            .header("Accept", "application/json")
+            .header("Content-Type", "application/sparql-query")
+            .response(function(xhr) {
+                return xhr.responseText;
+            })
+            .post(getRelationships(d.id), function(data) {
+                //console.log(data);
+                sparql_response = JSON.parse(data);
+                results = sparql_response.results.bindings;
+                new_nodes = [];
+                new_links = [];
+                //console.log(results);
+                for (k in results) {
+                    new_node = results[k];
+                    if (new_node.s) {
+                        id = new_node.s.value;
+                        label = new_node.s_label.value;
+                        type = new_node.s_type.value;
+                    } else {
+                        id = new_node.o.value;
+                        label = new_node.o_label.value;
+                        type = new_node.o_type.value
                     }
-                    for (new_node in new_nodes) {
-                        if (!unique_nodes[new_nodes[new_node].id]) {
-                            unique_nodes[new_nodes[new_node].id] = new_nodes[new_node];
-                            nodes.push(new_nodes[new_node]);
-                        }
+                    //console.log(id);
+                    //console.log(label);
+                    //console.log(type);
+
+                    i = groups[type].id;
+                    r = 20;
+
+                    new_nodes[id] = {
+                        "id": id,
+                        "group": groups[type].id,
+                        "type": type,
+                        "img": groups[type].img,
+                        "label": label,
+                        "radius": r,
+                        "cluster": i
+                    };
+                    if (!clusters[i] || (r > clusters[i].radius)) clusters[i] = new_nodes[id];
+                }
+                for (new_node in new_nodes) {
+                    if (!unique_nodes[new_nodes[new_node].id]) {
+                        unique_nodes[new_nodes[new_node].id] = new_nodes[new_node];
+                        nodes.push(new_nodes[new_node]);
                     }
-                    update();
-                });
+                }
+                update();
             });
+        })
+        .merge(circle);
+
     	// Icon ENTER
 	icon = icon.data(nodes)
 	    .enter().append("svg:image")
             .attr("class", "icon")
             .attr("xlink:href", function(d) { return d.img; })
             .attr("width", "20")
-            .attr("height", "20");
+            .attr("height", "20")
+            .merge(icon);;
     icon.append("title")
         .text(function(d) { return d.id; });
 
+    console.log(clusters);
     console.log(nodes);
+    console.log(unique_nodes);
 }
-
-
-//    TODO: Update for v4
-//    .call(force.drag);
 
 function tick() {
     circle
@@ -184,8 +185,12 @@ function tick() {
 }
 
 function forceCluster(alpha) {
+
   for (var i = 0, n = nodes.length, node, cluster, k = alpha * 1; i < n; ++i) {
     node = nodes[i];
+    if (node.id == "http://library.temple.edu/tul") {
+        //console.log(node);
+    }
     cluster = clusters[node.cluster];
     node.vx -= (node.x - cluster.x) * k;
     node.vy -= (node.y - cluster.y) * k;
@@ -233,9 +238,8 @@ for (node in graph.nodes) {
     graph.nodes[node].cluster = i;
     graph.nodes[node].radius = r;
     nodes = graph.nodes;
+    unique_nodes[nodes[node].id] = nodes[node];
     if (!clusters[i] || (r > clusters[i].radius)) clusters[i] = nodes[node];
 }
-console.log(nodes);
-
 
 update();
